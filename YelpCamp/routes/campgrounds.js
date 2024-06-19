2be3c4cd214d1771105d03c4ba46b8b95e799574
Faheem <faheemuddinsayyed789@gmail.com>
2024-06-19 19:35:09 +0530

    AFTER RESTRUCTURING

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -6,20 +6,19 @@ const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 // REQUIRING CONTROLLERS:
 const campgrounds = require('../controllers/campgrounds');
 
-
-router.get('/', catchAsync(campgrounds.index));
+// FANCY WAY TO RESTRUCTURE ROUTES: ****ORDER MATTERS HERE TOO*****
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    // Using JOI library for comprehensive server side validations (validateCampground):
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
-// Using JOI library for comprehensive server side validations:
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
